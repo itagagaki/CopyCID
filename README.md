c3fdde7ebab67fe5bd944126cd422ffd0e3f60a5
itagagaki <itagagaki@gmail.com>
2024-10-13 14:15:05 +0900

    Add CID buttons to the place details field

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Places registered in Google Maps are assigned a Customer ID (CID) identifier, wh
 
 This extension adds a "Copy CID to clipboard" item to the context menu when Google Maps is open. It will copy the CID to the clipboard if it exists in the URL of the map.
 
+Version 1.1 adds a CID display to the place details field, in addition to the traditional method of using the extension's button or context menu, so that it can be copied to the clipboard by simply clicking on it, just like an address or phone number, etc.
+
 ## How to install
 Install from the store below or download this repository and install in developer mode.
 
@@ -16,6 +18,8 @@ Install from the store below or download this repository and install in develope
 Googleマップに登録されている場所にはCID（Customer ID）という識別子が付与されており、Googleマップを開く際にURLパラメータとして与えることで、その場所のマップを表示させることができます。
 
 この拡張機能は、Googleマップを開いているとき、コンテキストメニューに「CIDをクリップボードにコピー」という項目を追加します。その項目をクリックすると、マップのURLにCIDが存在していればそれをクリップボードにコピーします。
+
+Version 1.1 では、拡張機能のボタンやコンテキストメニューを利用する従来の方法に加えて、場所の詳細情報の欄にCIDの表示も追加され、住所や電話番号などと同様にそこをクリックするだけでクリップボードにコピーできるようになりました。
 
 ## インストール方法
 下記ストアからインストールするか、またはこのリポジトリをダウンロードしてデベロッパーモード／開発者モードでインストールしてください。
---
+++ page.js
@@ -0,0 +1,235 @@
+( () => {
+
+  let cid = '';
+  let cidButtonBox = null;
+  let alertBox = null;
+  let alertTimer = null;
+  let tooltipTransitionClass = null;
+  let observer = null;
+
+  const removeAlertBox = () => {
+    if (alertBox) {
+      document.body.removeChild(alertBox);
+      alertBox = null;
+    }
+  };
+
+  const alertCopied = () => {
+    if (typeof alertTimer === 'number') {
+      clearTimeout(alertTimer);
+      alertTimer = null;
+      removeAlertBox();
+    }
+    const text = document.createElement('span');
+    text.innerHTML = chrome.i18n.getMessage('copied');
+    text.style.fontSize = '13px';
+    alertBox = document.createElement('div');
+    alertBox.style.display = 'flex';
+    alertBox.style.justifyContent = 'center';
+    alertBox.style.alignItems = 'center';
+    alertBox.style.backgroundColor = 'black';
+    alertBox.style.color = 'white';
+    alertBox.style.position = 'fixed';
+    alertBox.style.borderRadius = '3px';
+    if (cidButtonBox) {
+      const bounds = cidButtonBox.getBoundingClientRect();
+      alertBox.style.top = bounds.top + 'px';
+      alertBox.style.left = bounds.right + 10 + 'px';
+      alertBox.style.height = bounds.height + 'px';
+      alertBox.style.width = '250px';
+    }
+    alertBox.appendChild(text);
+    document.body.appendChild(alertBox);
+    alertTimer = setTimeout(removeAlertBox, 1000);
+  };
+
+  const copyCID = () => {
+    const textarea = document.createElement('textarea');
+    textarea.style.position = 'fixed';
+    textarea.style.left = '-100%';
+    textarea.value = cid;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alertCopied();
+  };
+
+  const copyCIDasHTML = () => {
+    const textarea = document.createElement('textarea');
+    textarea.style.position = 'fixed';
+    textarea.style.left = '-100%';
+    const urlobj = new URL(document.location.href);
+    const hostname = /\bgoogle\./.exec(urlobj.hostname) ? urlobj.hostname : 'www.google.com';
+    textarea.value = 'https://'+hostname+'/maps?cid='+cid;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alertCopied();
+  };
+
+  const mouseEnter = (e) => {
+    const tooltip = document.querySelector('.goog-tooltip');
+    if (tooltip) {
+      tooltip.innerHTML = e.target.getAttribute('data-tooltip');
+      const bounds = e.target.getBoundingClientRect();
+      tooltip.style.top = (bounds.top + bounds.bottom) / 2 + 20 + 'px';
+      tooltip.style.left = bounds.left + 10 + 'px';
+      tooltip.style.visibility = 'visible';
+      if (tooltipTransitionClass) {
+        tooltip.classList.remove(tooltipTransitionClass);
+      }
+      tooltip.dispatchEvent(new MouseEvent("mouseover"));
+    }
+  };
+
+  const mouseLeave = (e) => {
+    const tooltip = document.querySelector('.goog-tooltip');
+    if (tooltip) {
+      tooltip.style.visibility = 'hidden';
+      if (tooltipTransitionClass) {
+        tooltip.classList.add(tooltipTransitionClass);
+      }
+      tooltip.dispatchEvent(new MouseEvent("mouseout"));
+    }
+  };
+
+  const build = () => {
+
+    // Detect CID
+    let matches = document.location.href.match(/[\?\&]cid=(\d+)/);
+    if (matches && matches[1]) {
+      cid = matches[1];
+    } else {
+      matches = [...document.location.href.matchAll(/!3m\d+!1s(?:0x[0-9A-Fa-f]+):(0x[0-9A-Fa-f]+)/g)];
+      const hexcid = matches[matches.length - 1];
+      if (hexcid && hexcid[1]) {
+        cid = BigInt(hexcid[1]).toString(10);
+      } else {
+        return;
+      }
+    }
+
+    // Find name of class for tooltip transition.
+    if (!tooltipTransitionClass) {
+      const tooltip = document.querySelector('.goog-tooltip');
+      if (tooltip) {
+        const cl = tooltip.classList;
+        if (cl && cl.length >= 3) {
+          tooltipTransitionClass = cl.item(2);
+        }
+      }
+    }
+
+    // Want to add a 'CID' button above the 'Plus Code' button.
+    // So first look for the BUTTON element.
+    const plusCodeButton = document.body.querySelector('button[data-item-id="oloc"]');
+    if (!plusCodeButton) {
+      return;
+    }
+    // The actual addition is as a sibling of the container of that button element.
+    // So make sure that the container and its parent exist.
+    const plusCodeContainer = plusCodeButton.parentNode;
+    const parent = plusCodeContainer?.parentNode;
+    if (!parent) {
+      return;
+    }
+    // If the CID button has already been added, then do nothing.
+    if (parent.querySelector('#cidbox')) {
+      return;
+    }
+
+    // Build CID button.
+    cidButtonBox = plusCodeContainer.cloneNode(true);
+    cidButtonBox.setAttribute('id', 'cidbox');
+
+    const cidButton = cidButtonBox.querySelector('button:first-of-type');
+    cidButton.removeAttribute('jsaction');
+    cidButton.removeAttribute('jslog');
+    cidButton.removeAttribute('aria-label');
+    cidButton.removeAttribute('data-item-id');
+    cidButton.setAttribute('data-tooltip', chrome.i18n.getMessage('copy_CID'));
+    cidButton.onclick = copyCID;
+    cidButton.addEventListener('mouseenter', mouseEnter);
+    cidButton.addEventListener('mouseleave', mouseLeave);
+
+    const cidButtonIcon = cidButton.querySelector('div:first-of-type > div:nth-of-type(1) > span:first-of-type');
+    cidButtonIcon.innerHTML = 'CID';
+    cidButtonIcon.setAttribute('style', 'font-family: roboto; font-size: 15px; font-weight: bold; line-height: 24px; width: 24px; text-align: center;');
+
+    const cidButtonText = cidButton.querySelector('div:first-of-type > div:nth-of-type(2) > div:first-of-type');
+    cidButtonText.innerHTML = cid;
+    cidButtonText.setAttribute('style', 'line-height: 24px;');
+
+    // Add more sub-buttons.
+    const subbuttonsContainer = cidButton.nextSibling?.firstChild;
+    if (subbuttonsContainer) {
+      // For the first sub-button, assume it is a copy button and reuse it.
+      const button1Container = subbuttonsContainer.firstChild;
+      const button1 = button1Container?.firstChild;
+      if (button1) {
+        button1.setAttribute('jsaction', 'focus:pane.focusTooltip;blur:pane.blurTooltip');
+        button1.removeAttribute('jsaction');
+        button1.removeAttribute('jslog');
+        button1.removeAttribute('aria-label');
+        button1.removeAttribute('data-item-id');
+        button1.removeAttribute('data-value');
+        button1.setAttribute('data-tooltip', chrome.i18n.getMessage('copy_CID'));
+        button1.onclick = copyCID;
+        button1.addEventListener('mouseenter', mouseEnter);
+        button1.addEventListener('mouseleave', mouseLeave);
+      }
+      // Remove the second and subsequent ones.
+      let bc = button1Container;
+      while ((bc = bc.nextSibling) != null) {
+        subbuttonsContainer.removeChild(bc);
+      }
+      // Add 'Copy as HTML code' button.
+      const button2Container = button1Container.cloneNode(true);
+      const button2 = button2Container?.firstChild;
+      if (button2) {
+        const button2Icon = button2.firstChild?.querySelector('span:first-of-type');
+        if (button2Icon) {
+          button2Icon.innerHTML = 'URL';
+          button2Icon.setAttribute('style', 'font-size: 10px; line-height: 18px; width: 18px; height: 18px;');
+        }
+        button2.setAttribute('data-tooltip', chrome.i18n.getMessage('copy_URL'));
+        button2.onclick = copyCIDasHTML;
+        button2.addEventListener('mouseenter', mouseEnter);
+        button2.addEventListener('mouseleave', mouseLeave);
+      }
+      subbuttonsContainer.insertBefore(button2Container, null);
+    }
+
+    // Add the CID button to the page.
+    parent.insertBefore(cidButtonBox, plusCodeContainer);
+  };
+
+  /*
+   *  Start.
+   */
+  window.onload = () => {
+    observer = new MutationObserver(build);
+    observer.observe(document.body, { childList: true, subtree: true });
+  };
+
+  /*
+   *  Clean up and stop working if this extension is disabled or deleted.
+   */
+  const onDisabled = () => {
+    observer?.disconnect();
+    const cidbox = document.body.querySelector('#cidbox');
+    cidbox?.parentNode?.removeChild(cidbox);
+  }
+
+  let pingTimer;
+  const validityChecker = () => {
+    if (chrome.runtime?.id) {
+      return;
+    }
+    clearInterval(pingTimer);
+    onDisabled();
+  };
+  pingTimer = setInterval(validityChecker, 1500);
+})();
